387d8f39b51682a109e0e47454d03265e6353ef7
haicauvn <haidv245@gmail.com>
2021-11-17 21:18:37 +0700

    change idea

--- js/script.js
+++ js/script.js
@@ -2,36 +2,58 @@
 // ======
 
 window.addEventListener('load', function() {
-    var btnBolds = document.querySelectorAll('.btn.btn-bold');
-
-    var btnItalics = document.querySelectorAll('.btn.btn-italic');
 
     var btnSendLeft = document.querySelector('#btn-send-left')
     var btnSendRight = document.querySelector('#btn-send-right')
 
-    var btnReset = document.querySelector('.btn.btn-reset');
-    
     var inputLeft = document.querySelector('#btn-input-left');
+    var inputRight = document.querySelector('#btn-input-right');
 
-    btnBolds.forEach(e => {
-        e.addEventListener('click', function(event) {
-            e.classList.toggle('active');
-            event.preventDefault();
-            document.execCommand('bold');
-        })
-    });
+    var btnBoldLeft = document.querySelector('.btn-bold-left');
+    var btnItalicLeft = document.querySelector('.btn-italic-left');
+    var btnResetLeft = document.querySelector('.btn-reset-left');
 
+    var btnBoldRight = document.querySelector('.btn-bold-right');
+    var btnItalicRight = document.querySelector('.btn-italic-right');
+    var btnResetRight = document.querySelector('.btn-reset-right');
     
-    btnItalics.forEach(e => {
-        e.addEventListener('click', function(event) {
-            e.classList.toggle('active');
-            event.preventDefault();
-            document.execCommand('italic');
-        })
-    });
+    btnBoldLeft.addEventListener('click', function() {
+        btnBoldLeft.classList.toggle('active');
+        if (btnBoldLeft.classList.contains('active')) {
+            inputLeft.innerHTML = `<b>${inputLeft.innerHTML}</b>`
+        } else {
+            let inputParent = inputLeft.querySelector('b').parentElement;
+            inputParent.innerHTML = inputParent.querySelector('b').innerHTML;
+        }
+        document.querySelector('#btn-input-left').focus();
+    })
+
+    btnItalicLeft.addEventListener('click', function() {
+        btnItalicLeft.classList.toggle('active');
+        if (btnItalicLeft.classList.contains('active')) {
+            inputLeft.innerHTML = `<i>${inputLeft.innerHTML}</i>`
+        } else {
+            let inputParent = inputLeft.querySelector('i').parentElement;
+            inputParent.innerHTML = inputParent.querySelector('i').innerHTML;
+        }
+        document.querySelector('#btn-input-left').focus();
+    })
+
+    var resetLeft = function() {
+        btnBoldLeft.classList.remove('active');
+        btnItalicLeft.classList.remove('active');
+        document.querySelector('#btn-input-left').innerHTML = '';
+    }
+
+    btnResetLeft.addEventListener('click', function() {
+        resetLeft();
+    })
 
     btnSendLeft.addEventListener('click', function() {
         var inputLeft = document.querySelector('#btn-input-left');
+        if(inputLeft.innerText.trim() === '') {
+            return;
+        }
         var e1 = document.createElement('div');
         e1.classList.add('col-sm-11');
         var msgContent = inputLeft.innerHTML
@@ -48,11 +70,51 @@ window.addEventListener('load', function() {
             <div class="msg msg-right">
                 ${msgContent}
             </div>`;
+        
+        
         document.querySelector('#msg-chat-right').appendChild(e2);
+        resetLeft();
+    })
+
+
+    // Right ChatBox
+    btnBoldRight.addEventListener('click', function() {
+        btnBoldRight.classList.toggle('active');
+        if (btnBoldRight.classList.contains('active')) {
+            inputRight.innerHTML = `<b>${inputRight.innerHTML}</b>`
+        } else {
+            let inputParent = inputRight.querySelector('b').parentElement;
+            inputParent.innerHTML = inputParent.querySelector('b').innerHTML;
+        }
+        document.querySelector('#btn-input-right').focus();
+    })
+
+    btnItalicRight.addEventListener('click', function() {
+        btnItalicRight.classList.toggle('active');
+        if (btnItalicRight.classList.contains('active')) {
+            inputRight.innerHTML = `<i>${inputRight.innerHTML}</i>`
+        } else {
+            let inputParent = inputRight.querySelector('i').parentElement;
+            inputParent.innerHTML = inputParent.querySelector('i').innerHTML;
+        }
+        document.querySelector('#btn-input-right').focus();
+    })
+    
+    var resetRight = function() {
+        btnBoldRight.classList.remove('active');
+        btnItalicRight.classList.remove('active');
+        document.querySelector('#btn-input-right').innerHTML = '';
+    }
+
+    btnResetRight.addEventListener('click', function() {
+        resetRight();
     })
 
     btnSendRight.addEventListener('click', function() {
         var inputRight = document.querySelector('#btn-input-right');
+        if(inputRight.innerText.trim() === '') {
+            return;
+        }
         var e1 = document.createElement('div');
         e1.classList.add('col-sm-11');
         var msgContent = inputRight.innerHTML
@@ -70,16 +132,10 @@ window.addEventListener('load', function() {
                 ${msgContent}
             </div>`;
         document.querySelector('#msg-chat-left').appendChild(e2);
-        
-        
-
-    })
-
-    document.querySelector('#btn-reset-left').addEventListener('click', function() {
-        document.querySelector('#btn-input-left').innerHTML = '';
     })
 
     document.querySelector('#btn-reset-right').addEventListener('click', function() {
         document.querySelector('#btn-input-right').innerHTML = '';
     })
+    resetLeft();
 })
